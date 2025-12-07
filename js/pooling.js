faa17f0487412735514068f15ebcfdb31a819dd1
abaalfadeel <abaalfadeel1@gmail.com>
2025-12-07 11:45:37 +0300

    Add generic object pool class
    Implemented a generic object pool class for optimization.

--- js/pooling.js
+++ js/pooling.js
@@ -1,3 +1,15 @@
-export function getFromPool(type) {
-  // Object pool for optimization
+// pooling.js - generic object pool
+class Pool {
+  constructor(factory, initial = 10){
+    this.factory = factory;
+    this.items = [];
+    for(let i=0;i<initial;i++) this.items.push(this.factory());
+  }
+  acquire(){
+    return this.items.length ? this.items.pop() : this.factory();
+  }
+  release(obj){
+    if(obj.reset) obj.reset();
+    this.items.push(obj);
+  }
 }
