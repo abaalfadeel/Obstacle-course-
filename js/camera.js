54a3a0c4d6ee3b529b85b9d13320061827270afc
abaalfadeel <abaalfadeel1@gmail.com>
2025-12-07 11:45:59 +0300

    Update camera.js

--- js/camera.js
+++ js/camera.js
@@ -1,3 +1,11 @@
-export function updateCamera() {
-  // Camera movement logic
-}
+// camera.js - simple 2D camera parallax
+const Camera = {
+  container: null,
+  init(stage){
+    this.container = new PIXI.Container();
+    // We'll put world children under camera.container
+    stage.addChild(this.container);
+  },
+  add(child){ this.container.addChild(child); },
+  update(dt){ /* placeholder for shake / lerp */ }
+};
