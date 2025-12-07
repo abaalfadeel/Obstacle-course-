3fd7c190a9973d1cc5d08f7fbd90b50dd0b35d03
abaalfadeel <abaalfadeel1@gmail.com>
2025-12-07 11:47:32 +0300

    Update pickup.js

--- js/entities/pickup.js
+++ js/entities/pickup.js
@@ -1,4 +1,22 @@
-export class Pickup {
-  update() {}
-  render() {}
+// pickup.js - coin pickup
+class Pickup {
+  constructor(){
+    this.container = new PIXI.Container();
+    const g = new PIXI.Graphics();
+    g.beginFill(0xffcc00); g.drawCircle(0,0,18); g.endFill();
+    this.container.addChild(g);
+    Camera.add(this.container);
+    this.active = false;
+  }
+  spawn(lane, y){
+    const lanesX = [120,360,600];
+    this.container.x = lanesX[lane];
+    this.container.y = y;
+    this.active = true; this.container.visible = true;
+  }
+  collect(){
+    this.active = false; this.container.visible = false;
+    AudioManager.play('coin');
+  }
+  reset(){ this.active = false; this.container.visible = false; }
 }
