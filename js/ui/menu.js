a506cd9d135fcf6ee06b1552b6bfe52ebb4b53e7
abaalfadeel <abaalfadeel1@gmail.com>
2025-12-07 11:48:20 +0300

    Refactor menu.js to use Menu object for UI control

--- js/ui/menu.js
+++ js/ui/menu.js
@@ -1,3 +1,14 @@
-export function showMenu() {
-  // Show main menu
-}
+// menu.js
+const Menu = {
+  el: null, btnStart: null, btnStore: null,
+  init(){
+    this.el = document.getElementById('menu');
+    this.btnStart = document.getElementById('btn-start');
+    this.btnStore = document.getElementById('btn-store-open');
+    this.btnStart.addEventListener('click', ()=> { this.hide(); Game.start(); });
+    this.btnStore.addEventListener('click', ()=> { UI.showStore(); });
+    this.show();
+  },
+  show(){ this.el.classList.remove('hidden'); },
+  hide(){ this.el.classList.add('hidden'); }
+};
